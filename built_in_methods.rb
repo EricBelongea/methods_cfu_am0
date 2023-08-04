f5e1faa1e24b0b653644279e45b78068671eae09
Eric Belongea <eric.belongea@gmail.com>
2023-08-04 14:28:17 -0600

    Add answers to methods section 3

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -4,50 +4,50 @@
 
 # EXAMPLE
 # The downcase method is called on the string object "Hello World"
-# # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
-# # The return value is "hello world"
+# No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
+# The return value is "hello world"
 
-# puts "Hello World".downcase
+puts "Hello World".downcase
 
-# # The include? method is called on the string object "Hello World". One arguement is passed "Hello"
-# # The goal is to see if the arguement string object exists within the original string object. 
-# # .include? can be called on both arrays and strings.This returns a boolean value of either true 
-# # or false. In this case true. 
+# The include? method is called on the string object "Hello World". One arguement is passed "Hello"
+# The goal is to see if the arguement string object exists within the original string object. 
+# .include? can be called on both arrays and strings.This returns a boolean value of either true 
+# or false. In this case true. 
 
-# puts "Hello World".include?("Hello")
+puts "Hello World".include?("Hello")
 
-# # The .end_with? method passes one arguement which is called upon the string object. 
-# # I assume that the .end_with? looks a the length of the argument (5) then looks at the last 
-# # five index of the string object it is being called upon. ('World'). These two do not match 
-# # so the method returns a boolean value of false. 
+# The .end_with? method passes one arguement which is called upon the string object. 
+# I assume that the .end_with? looks a the length of the argument (5) then looks at the last 
+# five index of the string object it is being called upon. ('World'). These two do not match 
+# so the method returns a boolean value of false. 
 
-# puts "Hello World".end_with?("Hello")
+puts "Hello World".end_with?("Hello")
 
-# # Same method as above called on the same string object, but this time returns the boolean
-# # value of true. The last three index (length of argument ('rld')) match the last three index
-# # of the string object upon which the method is being called. I played around with this and 
-# # this method cannot be called on arrays or integers, so this must be a string only method. 
+# Same method as above called on the same string object, but this time returns the boolean
+# value of true. The last three index (length of argument ('rld')) match the last three index
+# of the string object upon which the method is being called. I played around with this and 
+# this method cannot be called on arrays or integers, so this must be a string only method. 
 
-# puts "Hello World".end_with?("rld")
-
-
-# # .even? is a method called on an integer and returns a boolean value of true is the integer is 
-# # infact even. false if odd
-
-# puts 12.even?
-
-# # The .next method works when iterating through code blocks. In the given example, 18.next the 
-# # method returns 19 which is the next number. next works like how we conceptualize 'skip'. 
-# # the example that I imported really helped me conceptualize what was going on in this method. 
-# # We next 'are skipping' all integers in an array that are less than or equal to two.
-
-# puts 18.next
+puts "Hello World".end_with?("rld")
 
 
-# [0,1,2,3,4].each do |n|
-#     next unless n > 2
-#     puts "Big number: #{n}"
-# end
+# .even? is a method called on an integer and returns a boolean value of true is the integer is 
+# infact even. false if odd
+
+puts 12.even?
+
+# The .next method works when iterating through code blocks. In the given example, 18.next the 
+# method returns 19 which is the next number. next works like how we conceptualize 'skip'. 
+# the example that I imported really helped me conceptualize what was going on in this method. 
+# We next 'are skipping' all integers in an array that are less than or equal to two.
+
+puts 18.next
+
+
+[0,1,2,3,4].each do |n|
+    next unless n > 2
+    puts "Big number: #{n}"
+end
 
 
 
@@ -109,9 +109,27 @@ puts num.to_s
 nums = 12
 puts nums.div(3)
 
-
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# when called on an array the .join method forms a new string by combining all elements in the array.
+# Using puts to see the result in the terminal allows us to see that the new string is created; however,
+# the new string is created without space between the elements. 'Ilikewaffles' or '1234'
+statement = ["I", "like", "waffles"]
+puts statement.join
+
+i_array = [1,2,3,4]
+puts i_array.join
+
+# When called upon an array the .count method returns an integer corresponding to the number of 
+# elements inside the array. Can be used with arrays containing integers or strings. 
+array = [1,2,3,4,5,6,7,8,9,10]
+puts array.count
+
+s_array = ["Bob", "Jim", "Jill", "Maddie"]
+puts s_array.count
+
+
